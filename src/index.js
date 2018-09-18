60be7c23d7a6eef1a0ff573bfc6305ea51d44531
evpodrezenko <evpodrezenko@gmail.com>
2018-09-18 13:01:17 +0300

    Count of the number of love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var numberLoveTriangles = 0;
+
+  for (var i = 0; i < preferences.length; i++) {
+    var a = preferences[i];
+    if (a === i+1) { // excludes reference to itself
+      continue;
+    }
+    var b = preferences[a-1];
+    var c = preferences[b-1];
+    if (c-1 === i) { // return to "a", because man "c" loves man "a"
+      numberLoveTriangles++;
+    }
+  }
+
+  // division by 3 excludes love triangles with combinations of the same elements
+  return numberLoveTriangles = numberLoveTriangles / 3; 
 };
